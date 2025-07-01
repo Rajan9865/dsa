050161e8a95a788e7dba66bb27e3495374cb5855
Rajan9865 <rajan06166@gmail.com>
2025-07-01 18:56:28 +0530

    hello

---
+++ DataStracture/src/string/Hello.java
@@ -0,0 +1,13 @@
+package string;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * DataStracture
+ * @since 7/1/2025
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
