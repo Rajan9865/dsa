baa8d49ca81576105bd4d1a9fcf7038ec07bc54f
Rajan <rajan78800@gmail.com>
2025-07-02 11:07:27 +0530

    even or odd

---
+++ DataStracture/src/linkedList/IsLinkedListLengthEven.java
@@ -0,0 +1,49 @@
+package linkedList;
+
+import javax.crypto.Cipher;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * DataStracture
+ * @since 7/2/2025
+ */
+public class IsLinkedListLengthEven {
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(10);
+        Node c = new Node(10);
+        Node d = new Node(10);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        boolean result = isLengthEven(a);
+        System.out.println(" even " + result);
+    }
+
+    private static boolean isLengthEven(Node head) {
+        Node current = head;
+        int count = 0;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        if (count % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    /**
+     * @param data
+     */
+    Node(int data) {
+        this.data = data;
+    }
+}
