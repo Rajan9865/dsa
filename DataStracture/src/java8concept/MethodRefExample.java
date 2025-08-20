7cfe2be26df9ebeec6779d44540e1edc3931aedf
Rajan9865 <rajan78800@gmail.com>
2025-08-20 12:38:36 +0530

    MethodRefExample

---
+++ DataStracture/src/java8concept/MethodRefExample.java
@@ -0,0 +1,21 @@
+package java8concept;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 8/20/2025
+ */
+public class MethodRefExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Rajan", "Kumar", "Java", "8");
+        methodsReferece(names);
+    }
+
+    private static void methodsReferece(List<String> names) {
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
+    }
+}
