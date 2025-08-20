4bd95d14c30ea17ebafa3c6094b24cc650d34df7
Rajan9865 <rajan78800@gmail.com>
2025-08-20 11:56:27 +0530

    LambdaFinalExample

---
+++ DataStracture/src/java8concept/LambdaFinalExample.java
@@ -0,0 +1,17 @@
+package java8concept;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 8/20/2025
+ */
+public class LambdaFinalExample {
+    public static void main(String[] args) {
+        String gretting = "Hello";
+//        gretting = "hi"; // This line will cause a compilation error if uncommented, as 'gretting' is effectively final.
+        Runnable runnable = () -> System.out.println(gretting);
+        runnable.run();
+//        System.out.println(gretting);
+    }
+}
