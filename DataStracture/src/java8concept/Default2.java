6cd1b76a70a8b3e3102e7d694227830b96a32064
Rajan9865 <rajan78800@gmail.com>
2025-08-22 18:40:52 +0530

    default methods example

--- DataStracture/src/java8concept/Default2.java
+++ DataStracture/src/java8concept/Default2.java
@@ -6,5 +6,33 @@ package java8concept;
  * Dsa
  * @since 8/22/2025
  */
+interface Calculator {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    default int multiply(int a, int b) {
+        return a * b;
+    }
+
+    void print();
+}
+
+class Something implements Calculator {
+
+    @Override
+    public void print() {
+        System.out.println("Hello, World!");
+    }
+}
+
 public class Default2 {
+    public static void main(String[] args) {
+        Calculator calculator = new Something();
+        calculator.print();
+        System.out.println("multiply: " + calculator.multiply(5, 10));
+        System.out.println("add: " + Calculator.add(5, 10));
+        // Using static method from interface
+        // Using default method from interface
+    }
 }
