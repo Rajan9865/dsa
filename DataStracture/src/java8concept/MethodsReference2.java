fdee99422e85bdc3c990face762a93cfac0955c9
Rajan9865 <rajan78800@gmail.com>
2025-08-22 11:31:24 +0530

    methods references2

---
+++ DataStracture/src/java8concept/MethodsReference2.java
@@ -0,0 +1,24 @@
+package java8concept;
+
+import java.util.function.Consumer;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 8/22/2025
+ */
+public class MethodsReference2 {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        MethodsReference2 methodsReference2 = new MethodsReference2();
+        Consumer<String> consumer = (s) -> methodsReference2.printMessage(s);
+        consumer.accept("Hello, World!  dear");
+//        Consumer<String> consumer1 = methodsReference2::printMessage;
+//        consumer1.accept("Hello, World!");
+
+    }
+}
