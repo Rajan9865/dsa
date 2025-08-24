e369d7e12b23af362b0c62769ad7839d0b8fc617
Rajan9865 <rajan78800@gmail.com>
2025-08-24 22:04:22 +0530

    innfosys intterview question

--- DataStracture/src/string/FrequencyCountAndMaintainTheAscendingOrder1.java
+++ DataStracture/src/string/FrequencyCountAndMaintainTheAscendingOrder1.java
@@ -1,10 +1,36 @@
 package string;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
- * @author lenovo
+ * @author Rajan kumar
  * @version 1.0
  * Dsa
  * @since 8/24/2025
  */
 public class FrequencyCountAndMaintainTheAscendingOrder1 {
+    public static void main(String[] args) {
+        String name = "rajankumar";
+        Map<Character, Long> output = frequencyCount(name);
+        System.out.println(output);
+        List<Map.Entry<Character, Long>> sortedList = sortedList(output);
+        printResult(sortedList);
+    }
+
+    private static void printResult(List<Map.Entry<Character, Long>> sortedList) {
+        sortedList.forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
+    }
+
+    private static List<Map.Entry<Character, Long>> sortedList(Map<Character, Long> output) {
+        return output.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toList());
+    }
+
+    private static Map<Character, Long> frequencyCount(String name) {
+        return name.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
 }
