c49f3e49f2dea64f93599076050dd1f588298a5b
Rajan9865 <rajan78800@gmail.com>
2025-08-25 12:01:29 +0530

    optional concept

--- DataStracture/src/java8concept/OptionalExample.java
+++ DataStracture/src/java8concept/OptionalExample.java
@@ -1,10 +1,23 @@
 package java8concept;
 
+import java.util.Optional;
+
 /**
- * @author lenovo
- * @since 8/24/2025
+ * @author Rajan kumar
  * @version 1.0
  * Dsa
+ * @since 8/24/2025
  */
 public class OptionalExample {
+    public static void main(String[] args) {
+        //empty optional
+        Optional<String> name = Optional.empty();
+//non empty optional
+        Optional<String> name1 = Optional.ofNullable(null);
+        Optional<String> name2 = Optional.of("rajan");
+        System.out.println(name);
+        System.out.println(name1);
+        System.out.println(name2);
+
+    }
 }
