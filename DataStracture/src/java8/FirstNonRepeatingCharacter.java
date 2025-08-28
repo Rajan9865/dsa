ef1e402e15686ad2858931833d2b8e7c8d9e11e6
Rajan9865 <rajan78800@gmail.com>
2025-08-28 16:30:42 +0530

    collectors example

--- DataStracture/src/java8/FirstNonRepeatingCharacter.java
+++ DataStracture/src/java8/FirstNonRepeatingCharacter.java
@@ -1,10 +1,35 @@
 package java8;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
- * @author lenovo
- * @since 8/25/2025
+ * @author Rajan kumar
  * @version 1.0
  * Dsa
+ * @since 8/25/2025
  */
 public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String input = "swiss";
+        Character result = firstNonRepeatingCharacter(input);
+        if (result != null) {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("All characters are repeating.");
+        }
+    }
+
+    private static Character firstNonRepeatingCharacter(String input) {
+        Map<Character, Long> charCountMap = input.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return input.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> charCountMap.get(c) == 1)
+                .findFirst()
+                .orElse(null);
+    }
 }
--- DataStracture/src/java8concept/CollectorsExammple.java
+++ DataStracture/src/java8concept/CollectorsExammple.java
@@ -1,10 +1,18 @@
 package java8concept;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
- * @author lenovo
- * @since 8/28/2025
+ * @author Rajan Kumar
  * @version 1.0
  * Dsa
+ * @since 8/28/2025
  */
 public class CollectorsExammple {
+    public static void main(String[] args) {
+        List<String> collect = Stream.of("rajan", "kumar", "singh").collect(Collectors.toList());
+        collect.forEach(s -> System.out.println(s));
+    }
 }
