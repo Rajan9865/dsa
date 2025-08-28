abcd47194e47fed5d30aac14156759070b2a4338
Rajan9865 <rajan78800@gmail.com>
2025-08-28 17:16:52 +0530

    collectors example

--- DataStracture/src/java8concept/CollectorsExammple.java
+++ DataStracture/src/java8concept/CollectorsExammple.java
@@ -2,6 +2,7 @@ package java8concept;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,5 +21,12 @@ public class CollectorsExammple {
         collect1.stream().forEach(s -> System.out.println(s));
         LinkedList<String> collect2 = Stream.of("a", "b", "c").collect(Collectors.toCollection(LinkedList::new));
         collect2.forEach(s -> System.out.println(s));
+
+        Map<Integer, String> collect3 = Stream.of("a", "bb", "ccc").collect(Collectors.toMap(String::length, s -> s));
+        collect3.entrySet().stream().forEach(s -> System.out.println(s));
+
+        Stream.of("a", "bb", "ccc", "dd").collect(Collectors.toMap(String::length, s -> s, (s1, s2) -> s1 + "," + s2))
+                .entrySet().stream().forEach(s -> System.out.println(s));
+
     }
 }
