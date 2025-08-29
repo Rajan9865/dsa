358cc31d12a7f1eb9e19b3b6986e2b4eb95cc618
Rajan9865 <rajan78800@gmail.com>
2025-08-29 13:08:10 +0530

    collectors example

--- DataStracture/src/java8concept/CollectorsExammple1.java
+++ DataStracture/src/java8concept/CollectorsExammple1.java
@@ -12,11 +12,18 @@ import java.util.stream.Stream;
  */
 public class CollectorsExammple1 {
     public static void main(String[] args) {
-        Map<Integer, String> collect = Stream.of("a", "bb", "ccc", "dd", "eee", "f", "ggg");
+        Stream<String> collect = Stream.of("a", "bb", "ccc", "dd", "eee", "f", "ggg");
+        Map<Integer, String> collect1 = Stream.of("a", "bb", "ccc", "dd", "eee", "f", "ggg")
+                .collect(Collectors.toMap(String::length, s -> s, String::concat));
+        Map<Integer, String> collect2 = Stream.of("a", "bb", "ccc", "dd", "eee", "f", "ggg")
+                .collect(Collectors.toMap(String::length, s -> s, (s1, s2) -> s1 + "," + s2));
         printResult(collect);
+        
+        printResult(collect1);
+        printResult(collect2);
     }
 
     private static void printResult(Map<Integer, String> collect) {
-        
+        collect.forEach((key, value) -> System.out.println("Key: " + key + " Value: " + value));
     }
 }
