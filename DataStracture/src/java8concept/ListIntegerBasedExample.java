cec6cd31d84e017a4f8d515e244c1b5b5227fee3
Rajan9865 <rajan78800@gmail.com>
2025-09-16 18:03:08 +0530

    collectors example

--- DataStracture/src/java8concept/ListIntegerBasedExample.java
+++ DataStracture/src/java8concept/ListIntegerBasedExample.java
@@ -2,6 +2,8 @@ package java8concept;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author lenovo
@@ -12,13 +14,23 @@ import java.util.List;
 public class ListIntegerBasedExample {
     public static void main(String[] args) {
         List<List<Integer>> number = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 4), Arrays.asList(5, 6, 7));
-        List<Number> flatList = flatList(number);
+        List<Integer> flatList = flatList(number);
+        partation(number);
         resultPrint(flatList);
     }
 
-    private static void resultPrint(List<Number> flatList) {
+    private static Map<Boolean, List<List<Integer>>> partation(List<List<Integer>> numbers) {
+//        return flatList.stream().flatMap(List::stream).collect(Collectors.partitioningBy(num -> num % 2 == 0));
+        return numbers.stream()
+                .flatMap(List::stream) // flatten first
+                .collect(Collectors.partitioningBy(num -> num % 2 == 0));
     }
 
-    private static List<Number> flatList(List<List<Integer>> number) {
+    private static void resultPrint(List<Integer> numbers) {
+        numbers.forEach(s -> System.out.print(" " + s));
+    }
+
+    private static List<Integer> flatList(List<List<Integer>> number) {
+        return number.stream().flatMap(List::stream).collect(Collectors.toList());
     }
 }
