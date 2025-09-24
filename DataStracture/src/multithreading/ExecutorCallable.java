ca38ddb11340eb3891ddc0a55ce04374d6bf42e2
Rajan9865 <rajan78800@gmail.com>
2025-09-24 23:03:28 +0530

    multhreading example

--- DataStracture/src/multithreading/ExecutorCallable.java
+++ DataStracture/src/multithreading/ExecutorCallable.java
@@ -1,8 +1,6 @@
 package multithreading;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @author Rajan kumar
@@ -11,12 +9,14 @@ import java.util.concurrent.Executors;
  * @since 9/24/2025
  */
 public class ExecutorCallable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Callable<String> task = () -> {
             Thread.sleep(1000);
             return "hello from" + Thread.currentThread().getName();
         };
-
+        Future<String> future = executorService.submit(task);
+        System.out.println("result =" + future.get());
+        executorService.shutdown();
     }
 }
