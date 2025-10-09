193be6778767e110be77e1bb467c0a44b639c607
Rajan9865 <rajan78800@gmail.com>
2025-10-09 11:09:20 +0530

    multhreading example

---
+++ DataStracture/src/multithreading/WithoutVolatile.java
@@ -0,0 +1,31 @@
+package multithreading;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 10/9/2025
+ */
+public class WithoutVolatile {
+    private static boolean running = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getState());
+        Thread thread = new Thread(() -> {
+            System.out.println("inside thread " + Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getState());
+            while (running) {
+
+            }
+            System.out.println("stopped");
+        });
+        thread.start();
+        Thread.sleep(1000);
+        running = false;
+        System.out.println("flag set to false");
+        System.out.println(thread.getState());
+        System.out.println(thread.getName());
+
+    }
+}
