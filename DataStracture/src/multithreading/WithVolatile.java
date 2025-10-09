684ebcb04b1269d9f833d662af61e803cea60d22
Rajan9865 <rajan78800@gmail.com>
2025-10-09 11:16:53 +0530

    multhreading example

---
+++ DataStracture/src/multithreading/WithVolatile.java
@@ -0,0 +1,25 @@
+package multithreading;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 10/9/2025
+ */
+public class WithVolatile {
+    private static volatile boolean running = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (running) {
+
+            }
+            System.out.println("thread stopped");
+        });
+        thread.start();
+        Thread.sleep(1000);
+        running = false;
+        System.out.println("flag set to flase ");
+
+    }
+}
