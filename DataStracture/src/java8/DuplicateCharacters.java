e919e368b63ba9cd83c20278f6e7238b22842978
Rajan9865 <rajan78800@gmail.com>
2025-11-28 16:04:50 +0530

    PalindromeFinder

---
+++ DataStracture/src/java8/DuplicateCharacters.java
@@ -0,0 +1,10 @@
+package java8;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 11/28/2025
+ */
+public class DuplicateCharacters {
+}
--- DataStracture/src/java8/PrimeNumbersFinder.java
+++ DataStracture/src/java8/PrimeNumbersFinder.java
@@ -16,6 +16,40 @@ public class PrimeNumbersFinder {
         List<Integer> number = Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13);
         List<Integer> output = primeNumber(number);
         System.out.println(output);
+        List<Integer> output1 = primeNumberBasic(number);
+        List<Integer> output2 = primeNumberBasic1(number);
+        System.out.println(output1);
+        System.out.println(output2);
+    }
+
+    private static List<Integer> primeNumberBasic1(List<Integer> number) {
+        return number.stream().filter(PrimeNumbersFinder::isOptimizePrimeNumber)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isOptimizePrimeNumber(Integer number) {
+        if (number < number) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Integer> primeNumberBasic(List<Integer> number) {
+        return number.stream().filter(PrimeNumbersFinder::isPrimeBasic)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isPrimeBasic(Integer number) {
+        if (number < 1) return false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static List<Integer> primeNumber(List<Integer> number) {
