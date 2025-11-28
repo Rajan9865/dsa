2783bbed9c00656407b6bc4033cede4618e409aa
Rajan9865 <rajan78800@gmail.com>
2025-11-28 17:56:21 +0530

    PalindromeFinder

---
+++ DataStracture/src/linkedList/RemoveLoopFromLinkedList.java
@@ -0,0 +1,48 @@
+package linkedList;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 11/28/2025
+ */
+public class RemoveLoopFromLinkedList {
+    public static void main(String[] args) {
+        Node19 a = new Node19(10);
+        Node19 b = new Node19(20);
+        Node19 c = new Node19(30);
+        Node19 d = new Node19(40);
+        Node19 e = new Node19(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = c;
+        removeLoop(a);
+        printLinkedList(a);
+    }
+
+    private static void printLinkedList(Node19 a) {
+    }
+
+    private static void removeLoop(Node19 head) {
+        if (head == null || head.next == null) return;
+        Node19 slow = head;
+        Node19 fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null && fast.next == null) return;
+    }
+}
+
+class Node19 {
+    int data;
+    Node19 next;
+
+    Node19(int data) {
+        this.data = data;
+    }
+}
