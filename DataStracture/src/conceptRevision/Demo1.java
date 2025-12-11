811cf1ed75ef3f814490b7a450bc94c9dd472b0b
Rajan9865 <rajan78800@gmail.com>
2025-12-11 17:42:09 +0530

    thread

--- DataStracture/src/conceptRevision/Demo1.java
+++ DataStracture/src/conceptRevision/Demo1.java
@@ -8,12 +8,23 @@ package conceptRevision;
  */
 public class Demo1 {
     public static void main(String[] args) {
+        /*
         Thread thread = new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 System.out.println(i + " " + Thread.currentThread().getName());
             }
         });
         thread.start();
+        System.out.println("Main thread finished");
+
+         */
+        Runnable task = () -> {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(Thread.currentThread().getName() + " -> " + i);
+            }
+        };
+//        Thread thread1 = new Thread(task);
+//        thread1.start();
         System.out.println("Main thread finished");
     }
 }
