854dff6d4bf33b81864a07a1069cb52626c370e7
Rajan9865 <rajan78800@gmail.com>
2025-12-22 16:40:05 +0530

    ReverseLinkedList

---
+++ DataStracture/src/revision/ReverseKGroup.java
@@ -0,0 +1,51 @@
+package revision;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 12/19/2025
+ */
+public class ReverseKGroup {
+    public static void main(String[] args) {
+        Node3 a = new Node3(10);
+        Node3 b = new Node3(20);
+        Node3 c = new Node3(30);
+        Node3 d = new Node3(40);
+        Node3 e = new Node3(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        int k = 3;
+        Node3 result = reverseKGroup(a, k);
+        printlinkedList(result);
+    }
+
+    private static void printlinkedList(Node3 result) {
+
+    }
+
+    private static Node3 reverseKGroup(Node3 head, int k) {
+        Node3 current = head;
+        Node3 previous = null;
+        Node3 next = null;
+        int count = 0;
+        while (current != null && count < k) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            
+        }
+    }
+}
+
+class Node3 {
+    int data;
+    Node3 next;
+
+    public Node3(int data) {
+        this.data = data;
+    }
+
+}
