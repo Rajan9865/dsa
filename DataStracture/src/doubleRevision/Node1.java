0cec5d5cd25a02376fca382ece06f27cb82ee811
Rajan9865 <rajan78800@gmail.com>
2025-12-30 13:18:34 +0530

    ReverseLinkedList

---
+++ DataStracture/src/doubleRevision/Node1.java
@@ -0,0 +1,16 @@
+package doubleRevision;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 12/30/2025
+ */
+public class Node1 {
+    int data;
+    Node1 next;
+
+    Node1(int data) {
+        this.data = data;
+    }
+}
