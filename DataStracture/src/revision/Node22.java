628ab0ef0b60f9cb1c51f409d291ed75a33df6de
Rajan9865 <rajan78800@gmail.com>
2026-01-05 14:43:54 +0530

    ReverseLinkedList

---
+++ DataStracture/src/revision/Node22.java
@@ -0,0 +1,17 @@
+package revision;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 1/5/2026
+ */
+public class Node22 {
+    int data;
+    Node22 next;
+
+    Node22(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
