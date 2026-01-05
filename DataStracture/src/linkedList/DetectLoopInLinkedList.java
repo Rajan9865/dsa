dde199c4ad1b1a6ee2107f4d735da0c6d4600e8c
Rajan9865 <rajan78800@gmail.com>
2026-01-05 11:02:48 +0530

    ReverseLinkedList

---
+++ DataStracture/src/linkedList/DetectLoopInLinkedList.java
@@ -0,0 +1,42 @@
+package linkedList;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 1/2/2026
+ */
+public class DetectLoopInLinkedList {
+    public static void main(String[] args) {
+        Node22 a = new Node22(1);
+        Node22 b = new Node22(2);
+        Node22 c = new Node22(3);
+        Node22 d = new Node22(4);
+        Node22 e = new Node22(5);
+        Node22 f = new Node22(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+//        f.next = d;
+        boolean result = detectLoopInLinkedList(a);
+        System.out.println("is circular linkedlist - " + result);
+    }
+
+    private static boolean detectLoopInLinkedList(Node22 head) {
+        if (head == null) {
+            return false;
+        }
+        Node22 slow = head;
+        Node22 fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
