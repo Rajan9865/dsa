9c57a291a59678e00e51c89dc47fd810d0b88139
Rajan9865 <rajan78800@gmail.com>
2026-01-14 18:48:12 +0530

    ReverseLinkedList

--- DataStracture/src/linkedList/DetectLoopInLinkedList.java
+++ DataStracture/src/linkedList/DetectLoopInLinkedList.java
@@ -1,12 +1,35 @@
 package linkedList;
 
 /**
- * @author Rajan kumar
+ * Demonstrates how to detect a loop (cycle) in a singly linked list
+ * using Floyd’s Cycle Detection Algorithm (also known as the
+ * Tortoise and Hare algorithm).
+ *
+ * <p>
+ * The program constructs a linked list with a deliberate cycle
+ * and verifies whether the cycle exists by calling
+ * {@link #detectLoopInLinkedList(Node22)}.
+ * </p>
+ *
+ * <p>
+ * This approach runs in O(n) time and uses O(1) extra space.
+ * </p>
+ *
+ * @author Rajan Kumar
  * @version 1.0
- * Dsa
  * @since 1/2/2026
  */
 public class DetectLoopInLinkedList {
+    /**
+     * Entry point of the program.
+     *
+     * <p>
+     * Creates a sample linked list with a cycle and prints whether
+     * the linked list is circular.
+     * </p>
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         Node22 a = new Node22(1);
         Node22 b = new Node22(2);
@@ -20,6 +43,19 @@ public class DetectLoopInLinkedList {
         d.next = e;
         e.next = f;
         f.next = d;
+        /**
+         * Detects whether a given singly linked list contains a loop.
+         *
+         * <p>
+         * This method uses two pointers (slow and fast) that move through
+         * the list at different speeds. If the list contains a cycle,
+         * the two pointers will eventually meet.
+         * </p>
+         *
+         * @param head the head node of the linked list
+         * @return {@code true} if the linked list contains a loop,
+         *         {@code false} if the linked list is linear
+         */
         boolean result = detectLoopInLinkedList(a);
         System.out.println("is circular linkedlist - " + result);
     }
