a1a8dcf3a0ee3b6b50c1667b8c9a5cba0c9364c9
Rajan9865 <rajan78800@gmail.com>
2026-01-20 15:39:03 +0530

    ReverseLinkedList

---
+++ DataStracture/src/linkedList/PairwiseSwap.java
@@ -0,0 +1,53 @@
+package linkedList;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 1/20/2026
+ */
+public class PairwiseSwap {
+    public static void main(String[] args) {
+        Node22 a = new Node22(10);
+        Node22 b = new Node22(20);
+        Node22 c = new Node22(30);
+        Node22 d = new Node22(40);
+        Node22 e = new Node22(50);
+        Node22 f = new Node22(60);
+        Node22 g = new Node22(70);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        Node22 result = pairwiseSwap(a);
+        printLinkedList(result);
+
+    }
+
+    private static void printLinkedList(Node22 result) {
+        Node22 temp = result;
+        while (temp != null) {
+            System.out.print(temp.data + " ->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    private static Node22 pairwiseSwap(Node22 head) {
+        if (head == null) {
+            return null;
+        }
+        Node22 current = head;
+        int temp = 0;
+        while (current != null && current.next != null) {
+            temp = current.data;
+            current.data = current.next.data;
+            current.next.data = temp;
+
+            current = current.next.next;
+        }
+        return head;
+    }
+}
