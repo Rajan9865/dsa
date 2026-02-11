a707049190c8cb9f97b3b0bad89ab25a032266a2
Rajan9865 <rajan78800@gmail.com>
2026-02-11 11:21:11 +0530

    string rotation of given string

---
+++ DataStracture/src/string/StringRotation.java
@@ -0,0 +1,22 @@
+package string;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 2/11/2026
+ */
+public class StringRotation {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "cdab";
+        System.out.println(areRotation(s1, s2));
+    }
+
+    private static boolean areRotation(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String temp = s1 + s2;
+        System.out.println(s1 + " " + s2 + " " + temp);
+        return temp.contains(s2);
+    }
+}
