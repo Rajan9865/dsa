70cfc8f92a13173534007c00faa548cd30e285bd
Rajan9865 <rajan78800@gmail.com>
2026-03-19 17:22:23 +0530

    IsPalindrome this is the 2nd approach divided into two part from middle

--- DataStracture/src/string/IsPalindrome.java
+++ DataStracture/src/string/IsPalindrome.java
@@ -10,7 +10,26 @@ public class IsPalindrome {
     public static void main(String[] args) {
         String input = "abba";
         boolean results = isPalindrome(input);
-        System.out.println(results);
+        boolean results1 = isPalindromeApproach1(input);
+        boolean results2 = isPalindromeApproach2(input);
+        System.out.println(results2);
+    }
+
+    private static boolean isPalindromeApproach2(String input) {
+        return input.equals(new StringBuilder(input).reverse().toString());
+    }
+
+    private static boolean isPalindromeApproach1(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 
     private static boolean isPalindrome(String input) {
