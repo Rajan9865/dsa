677ef459b1ac74d78234a830956e5b4f2ada31c1
Rajan9865 <rajan78800@gmail.com>
2026-03-19 16:02:39 +0530

    IsPalindrome this is the first approach

---
+++ DataStracture/src/string/IsPalindrome.java
@@ -0,0 +1,24 @@
+package string;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 3/19/2026
+ */
+public class IsPalindrome {
+    public static void main(String[] args) {
+        String input = "abba";
+        boolean results = isPalindrome(input);
+        System.out.println(results);
+    }
+
+    private static boolean isPalindrome(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
