a173efe779e27a3a4c359c88f9d82cf379d234dd
Rajan9865 <rajan78800@gmail.com>
2026-03-20 11:31:23 +0530

    ReversePartialString string using third variable

---
+++ DataStracture/src/string/ReversePartialString.java
@@ -0,0 +1,33 @@
+package string;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 3/20/2026
+ */
+public class ReversePartialString {
+    public static void main(String[] args) {
+        String input = "abcdef";
+        int k = 3;
+        String result = reverseFirstK(input, k);
+        System.out.println(result);
+    }
+
+    private static String reverseFirstK(String input, int k) {
+        if (input == null || input.length() == 0 || k <= 1) {
+            return input;
+        }
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = Math.min(chars.length - 1, k - 1);
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return String.valueOf(chars);
+    }
+}
