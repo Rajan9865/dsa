b96eb616e22a6869c7a2fd9fefa99dc5c70315fa
Rajan9865 <rajan78800@gmail.com>
2026-03-20 12:51:40 +0530

    ReverseEachWord problem using no inbuilt methods

--- DataStracture/src/string/ReverseEachWord.java
+++ DataStracture/src/string/ReverseEachWord.java
@@ -9,6 +9,9 @@ package string;
 public class ReverseEachWord {
     public static void main(String[] args) {
         String input = "I Love java";
+//        String result = reverseEachWord(input);
+        String result1 = reverseEachWord1(input);
+        System.out.println("Reversed each String: " + result1);
         String[] words = input.toLowerCase().split(" ");
         StringBuilder stringBuilder = new StringBuilder();
         for (String word : words) {
@@ -16,5 +19,16 @@ public class ReverseEachWord {
             stringBuilder.append(stringBuilder1.reverse()).append(" ");
         }
         System.out.println(stringBuilder.toString().trim());
+    }
+
+    private static String reverseEachWord1(String input) {
+        String[] words = input.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                stringBuilder.append(word.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
     }
 }
