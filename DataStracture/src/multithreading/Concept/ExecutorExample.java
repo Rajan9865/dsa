22e944f6e8339b59c03c37bb09bbddc44c0d8441
Rajan9865 <rajan78800@gmail.com>
2026-03-30 19:15:32 +0530

    ExecutorExample

---
+++ DataStracture/src/multithreading/Concept/ExecutorExample.java
@@ -0,0 +1,20 @@
+package multithreading.Concept;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 3/30/2026
+ */
+public class ExecutorExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            System.out.println("running  in :" + Thread.currentThread().getName());
+        });
+        executorService.shutdown();
+    }
+}
