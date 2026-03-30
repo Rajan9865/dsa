a1b13f82c1d0ad2cb57a5d1ca9c9fccca5e9c311
Rajan9865 <rajan78800@gmail.com>
2026-03-30 19:15:46 +0530

    LockExample

---
+++ DataStracture/src/multithreading/Concept/LockExample.java
@@ -0,0 +1,13 @@
+package multithreading.Concept;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 3/30/2026
+ */
+public class LockExample {
+    public static void main(String[] args) {
+        
+    }
+}
