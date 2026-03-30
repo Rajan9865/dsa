e9dd7da200ac0b6cde1d41fdc77f8263c09a9a90
Rajan9865 <rajan78800@gmail.com>
2026-03-30 13:25:06 +0530

    MyThreadRunnable

---
+++ DataStracture/src/playground/MyThreadRunnable.java
@@ -0,0 +1,22 @@
+package playground;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 3/30/2026
+ */
+class MyThread1 implements Runnable {
+    public void run() {
+        System.out.println("MyThread run using implementation of runnable interface ");
+    }
+
+}
+
+public class MyThreadRunnable {
+    public static void main(String[] args) {
+//        Thread thread1 = new Thread(new MyThread1());
+//        thread1.start();
+        
+    }
+}
