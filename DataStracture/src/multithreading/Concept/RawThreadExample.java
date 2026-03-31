c9ad6b9167f02482bdc9c3fec1c74d177374cd58
Rajan9865 <rajan78800@gmail.com>
2026-03-31 10:43:42 +0530

    LockExample

---
+++ DataStracture/src/multithreading/Concept/RawThreadExample.java
@@ -0,0 +1,16 @@
+package multithreading.Concept;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 3/30/2026
+ */
+public class RawThreadExample {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            System.out.println("running  in :" + Thread.currentThread().getName());
+        });
+        t.start();
+    }
+}
