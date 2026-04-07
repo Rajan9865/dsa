7f40815555a3cbcb8037b3f0f03e366756f4f8a6
Rajan9865 <rajan78800@gmail.com>
2026-04-07 10:44:18 +0530

    completable fetures example

---
+++ DataStracture/src/multithreading/Concept/CompletableFuture/Example8.java
@@ -0,0 +1,24 @@
+package multithreading.Concept.CompletableFuture;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/3/2026
+ */
+public class Example8 {
+    public static void main(String[] args) {
+        CompletableFuture<Integer> future =
+                CompletableFuture.<Integer>supplyAsync(() -> {
+                            throw new RuntimeException("Error!");
+                        })
+                        .exceptionally(ex -> {
+                            System.out.println("Handled: " + ex.getMessage());
+                            return -1;
+                        });
+
+        System.out.println("Result: " + future.join());
+    }
+}
