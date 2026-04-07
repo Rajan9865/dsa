9e9dd81acc16f87ef54eda424dd2e387a6285643
Rajan9865 <rajan78800@gmail.com>
2026-04-07 18:07:15 +0530

    LivelockDemo

---
+++ DataStracture/src/multithreading/Concept/LivelockDemo.java
@@ -0,0 +1,36 @@
+package multithreading.Concept;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/7/2026
+ */
+public class LivelockDemo {
+    static volatile boolean flag = true;
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            while (flag) {
+                System.out.println("T1: giving chance to T2");
+                flag = false;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("T2: giving chance to T1");
+                flag = true;
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
