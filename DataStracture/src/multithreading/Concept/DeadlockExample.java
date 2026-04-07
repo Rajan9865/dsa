370aaa6f42fecb1f1eccedb502b5af0fb730e0b3
Rajan9865 <rajan78800@gmail.com>
2026-04-07 18:06:29 +0530

    DeadlockExample

---
+++ DataStracture/src/multithreading/Concept/DeadlockExample.java
@@ -0,0 +1,33 @@
+package multithreading.Concept;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/7/2026
+ */
+public class DeadlockExample {
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("t1 lock1 acquired");
+                synchronized (lock2) {
+                    System.out.println("t1 lock2 acquired");
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("t2 lock2 acquired");
+                synchronized (lock1) {
+                    System.out.println("t2 lock1 acquired");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
