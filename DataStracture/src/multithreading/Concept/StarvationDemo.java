03d0c2803f77f886b9f161bfd1de671b3ec53f6a
Rajan9865 <rajan78800@gmail.com>
2026-04-09 16:46:50 +0530

    StarvationDemo

---
+++ DataStracture/src/multithreading/Concept/StarvationDemo.java
@@ -0,0 +1,26 @@
+package multithreading.Concept;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/7/2026
+ */
+public class StarvationDemo {
+    public static void main(String[] args) {
+        Thread high = new Thread(() -> {
+            while (true) {
+                System.out.println("high priority running ..");
+            }
+        });
+        Thread low = new Thread(() -> {
+            while (true) {
+                System.out.println("low priority running ..");
+            }
+        });
+        high.setPriority(Thread.MAX_PRIORITY);
+        low.setPriority(Thread.MIN_PRIORITY);
+        high.start();
+        low.start();
+    }
+}
