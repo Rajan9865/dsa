10c714ba0c63172e238374ea0a572def9ee09c34
Rajan9865 <rajan78800@gmail.com>
2026-04-09 16:44:50 +0530

    DeadlockPreventUsingSamOrder

---
+++ DataStracture/src/multithreading/Concept/deadLockPrevent/DeadlockPreventUsingSamOrder.java
@@ -0,0 +1,49 @@
+package multithreading.Concept.deadLockPrevent;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/8/2026
+ */
+public class DeadlockPreventUsingSamOrder {
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("t1 acquired lock1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("t1 acquired lock2");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("t2 acquired lock1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("t2 acquired lock2");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
