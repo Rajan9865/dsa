54b40cf2a45d51c034de56e3ee46dc6a9a68a98d
Rajan9865 <rajan78800@gmail.com>
2026-04-10 11:45:23 +0530

    BasicLockExample

---
+++ DataStracture/src/multithreading/Concept/ReentrantLockExample/BasicLockExample.java
@@ -0,0 +1,51 @@
+package multithreading.Concept.ReentrantLockExample;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/9/2026
+ */
+class Counter {
+    private int count = 0;
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void inc() {
+        lock.lock();
+        try {
+            count++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class BasicLockExample {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.inc();
+            }
+        };
+        Thread t1 = new Thread(task, "t1");
+        Thread t2 = new Thread(task, "t2");
+        Thread t3 = new Thread(task, "t3");
+        Thread t4 = new Thread(task, "t4");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        System.out.println("final counter " + counter.getCount());
+    }
+}
