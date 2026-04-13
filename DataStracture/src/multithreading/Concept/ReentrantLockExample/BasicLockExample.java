4c9bb76ec651f16924366637743cb746fca2c825
Rajan9865 <rajan78800@gmail.com>
2026-04-13 10:13:18 +0530

    BasicLockExample

--- DataStracture/src/multithreading/Concept/ReentrantLockExample/BasicLockExample.java
+++ DataStracture/src/multithreading/Concept/ReentrantLockExample/BasicLockExample.java
@@ -36,16 +36,10 @@ public class BasicLockExample {
         };
         Thread t1 = new Thread(task, "t1");
         Thread t2 = new Thread(task, "t2");
-        Thread t3 = new Thread(task, "t3");
-        Thread t4 = new Thread(task, "t4");
         t1.start();
         t2.start();
-        t3.start();
-        t4.start();
         t1.join();
         t2.join();
-        t3.join();
-        t4.join();
         System.out.println("final counter " + counter.getCount());
     }
 }
