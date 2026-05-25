43d034e755300da211eae1f032eb9edcff9e5315
Rajan9865 <rajan78800@gmail.com>
2026-05-25 14:51:01 +0530

    CustomExecutorDemo

---
+++ DataStracture/src/multithreading/concept/completablefuture/CustomExecutorDemo.java
@@ -0,0 +1,13 @@
+package multithreading.concept.completablefuture;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 4/20/2026
+ */
+public class CustomExecutorDemo {
+    public static void main(String[] args) {
+        
+    }
+}
