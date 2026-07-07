76313f2852e07a64a35aeb06be0d76bfca0ba443
Rajan9865 <rajan78800@gmail.com>
2026-07-07 11:24:48 +0530

    third approach

---
+++ DataStracture/src/array/revision/SecondLargestElement.java
@@ -0,0 +1,21 @@
+package array.revision;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 7/6/2026
+ */
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] num = {1, 5, 7, 5, 9, 6};
+        System.out.println("1st approach " + findSecondLargestNumber1st(num));
+    }
+
+    private static int findSecondLargestNumber1st(int[] num) {
+        return Arrays.stream(num).boxed().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(-1);
+    }
+}
