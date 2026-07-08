eb7716331e1732828b29dacf02c69e09d5fa03b1
Rajan9865 <rajan78800@gmail.com>
2026-07-08 15:54:01 +0530

    third approach

---
+++ DataStracture/src/string/FirstNonRepeatingCharacter.java
@@ -0,0 +1,31 @@
+package string;
+
+import java.util.Scanner;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 7/8/2026
+ */
+public class FirstNonRepeatingCharacter {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("first non-repeating characters: " + firstNonRepeatingCharacter(str));
+    }
+
+    private static char firstNonRepeatingCharacter(String input) {
+        int[] freq = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            freq[input.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (freq[input.charAt(i) - 'a'] == 1) {
+                return input.charAt(i);
+            }
+        }
+        return '$';
+    }
+}
