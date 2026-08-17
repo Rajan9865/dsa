a0c4b7c4c43701ce2742bafaec1d6885416bcabd
Rajan9865 <rajan78800@gmail.com>
2026-08-17 13:02:45 +0530

    ReverseArrayBruteForce

---
+++ DataStracture/src/playground/A.java
@@ -0,0 +1,16 @@
+package playground;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 7/24/2026
+ */
+public class A {
+    static void main() {
+        String a = "hello";
+        a = a.concat("world");
+        System.out.println(a);
+        System.out.println(a.length());
+    }
+}
