4a9eade733223ed11ebcac93dffe68f42e731714
Rajan9865 <rajan78800@gmail.com>
2026-08-26 14:36:39 +0530

    Exception3

--- DataStracture/src/exceptions/Exception3.java
+++ DataStracture/src/exceptions/Exception3.java
@@ -8,13 +8,25 @@ package exceptions;
  */
 public class Exception3 {
     static void main() {
-        try {
+        /*try {
             int x = 10 / 0;
         } catch (ArithmeticException e) {
             System.out.println("Catch");
             throw new RuntimeException("No");
         } finally {
             System.out.println("Finally");
+        }*/
+        try {
+            try {
+                System.out.println("Inner Try");
+                int x = 10 / 0;
+            } finally {
+                System.out.println("Inner Finally");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Outer Catch");
+        } finally {
+            System.out.println("Outer Finally");
         }
     }
 }
