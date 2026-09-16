2683c051bfa9b074423924d83ab6ae4209fd5e34
Rajan9865 <rajan78800@gmail.com>
2026-09-16 12:26:41 +0530

    MergeSortedLinkedListsUsingTwoPointer

---
+++ DataStracture/src/top100dsa/MergeSortedLinkedListsUsingTwoPointer.java
@@ -0,0 +1,56 @@
+package top100dsa;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 9/16/2026
+ */
+public class MergeSortedLinkedListsUsingTwoPointer {
+    static void main() {
+        Node a = new Node(1);
+        Node b = new Node(3);
+        Node c = new Node(5);
+        a.next = b;
+        b.next = c;
+        Node a1 = new Node(3);
+        Node b1 = new Node(5);
+        Node c1 = new Node(6);
+        a1.next = b1;
+        b1.next = c1;
+        Node result = mergeLinkedList(a, a1);
+        System.out.println("merged linked list");
+        printlinkedList(result);
+    }
+
+    private static void printlinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    private static Node mergeLinkedList(Node head1, Node head2) {
+        Node dummy = new Node(0);
+        Node head = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                head.next = head1;
+                head1 = head1.next;
+            } else {
+                head.next = head2;
+                head2 = head2.next;
+            }
+            head = head.next;
+        }
+        if (head1 != null) {
+            head.next = head1;
+        }
+        if (head2 != null) {
+            head.next = head2;
+        }
+        return dummy.next;
+    }
+}
