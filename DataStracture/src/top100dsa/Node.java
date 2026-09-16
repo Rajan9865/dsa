5aa37bf5746e11676b45dfac403954fadfdd00f7
Rajan9865 <rajan78800@gmail.com>
2026-09-16 11:18:51 +0530

    node

---
+++ DataStracture/src/top100dsa/Node.java
@@ -0,0 +1,17 @@
+package top100dsa;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Dsa
+ * @since 9/16/2026
+ */
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
