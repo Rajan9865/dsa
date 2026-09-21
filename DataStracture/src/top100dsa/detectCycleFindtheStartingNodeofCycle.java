f88ffa61186fe003eee224c5da756af4e0c2b516
Rajan9865 <rajan78800@gmail.com>
2026-09-21 11:56:05 +0530

    detectCycleFindtheStartingNodeofCycle

---
+++ DataStracture/src/top100dsa/detectCycleFindtheStartingNodeofCycle.java
@@ -0,0 +1,43 @@
+package top100dsa;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Dsa
+ * @since 9/21/2026
+ */
+public class detectCycleFindtheStartingNodeofCycle {
+    static void main() {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+//        e.next = c;
+        Node result = detechCycleAndDindFirst(a);
+        System.out.println(result != null ? "cycle start at " + result.data : "no cycle found");
+    }
+
+    private static Node detechCycleAndDindFirst(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
